5003a97968b4847f28b365443ee8dbd21c558c52
Ballarja <JamesBallard87@hotmail.com>
2023-01-26 20:25:55 -0600

    tests added to BalanceBracketsTest

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,31 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
+
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        String testData = "";
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void checksForDoubleSquareBracket(){
+        String checkForDoubleBracket = "Checking for double bracket";
+        String testData = "[[]]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertEquals(checkForDoubleBracket, result, true);
+    }
+
+    @Test
+    public void checkForString(){
+        String launchCodeString = "Checking for Launch Code";
+        String testData = "";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        //String wordTested = "[]LaunchCode";
+        assertEquals(launchCodeString, result, true);
+    }
 
 }
